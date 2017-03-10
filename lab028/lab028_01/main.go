c80b64d77e6291217d8f3171e21fb3df587341d9
liguoqinjim <liguoqinjim23@gmail.com>
2017-03-10 14:51:59 +0800

    lab028_01 unsafe的使用

--- lab028/lab028_01/main.go
+++ lab028/lab028_01/main.go
@@ -18,19 +18,30 @@ func main() {
 	p := unsafe.Pointer(&s)
 	fmt.Println("p=", p)
 	up0 := uintptr(p)
+	fmt.Println("up0=", up0)
 	pb := (*byte)(p)
 	*pb = 10
 	fmt.Println(s)
 
 	//偏移到第二个字段
 	up := up0 + unsafe.Offsetof(s.b)
+	fmt.Println("up=", up)
 	p = unsafe.Pointer(up)
 	pb = (*byte)(p)
 	*pb = 20
 	fmt.Println(s)
 
+	//偏移到第三个字段
+	up = up0 + unsafe.Offsetof(s.c)
+	fmt.Println("up=", up)
+	p = unsafe.Pointer(up)
+	pb = (*byte)(p)
+	*pb = 50
+	fmt.Println(s)
+
 	//偏移到第四个字段
 	up = up0 + unsafe.Offsetof(s.d)
+	fmt.Println("up=", up)
 	p = unsafe.Pointer(up)
 	pi := (*int64)(p)
 	*pi = 40
