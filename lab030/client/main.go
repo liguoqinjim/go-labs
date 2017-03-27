aad43a9f816c189be0ca52dd4af80eea110b434f
liguoqinjim <liguoqinjim23@gmail.com>
2017-03-27 20:29:56 +0800

    socket

--- lab030/client/main.go
+++ lab030/client/main.go
@@ -2,8 +2,33 @@ package main
 
 import (
 	"fmt"
+	"net"
+	"os"
+	"time"
 )
 
 func main() {
-	fmt.Println("client")
+	server := "127.0.0.1:3333"
+	tcpAddr, err := net.ResolveTCPAddr("tcp4", server)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	conn, err := net.DialTCP("tcp", nil, tcpAddr)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println("connect success")
+	sender(conn)
+
+	time.Sleep(time.Second * 3)
+}
+
+func sender(conn net.Conn) {
+	words := "hello world!"
+	conn.Write([]byte(words))
+	fmt.Println("send over")
 }
--- lab030/server/main.go
+++ lab030/server/main.go
@@ -2,8 +2,45 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net"
+	"os"
 )
 
 func main() {
-	fmt.Println("server")
+	netListen, err := net.Listen("tcp", ":3333")
+	CheckError(err)
+	defer netListen.Close()
+
+	for {
+		conn, err := netListen.Accept()
+		if err != nil {
+			continue
+		}
+
+		log.Println(conn.RemoteAddr().String(), " tcp connect success")
+		handleConnnection(conn)
+	}
+}
+
+func handleConnnection(conn net.Conn) {
+	buffer := make([]byte, 2048)
+
+	for {
+		n, err := conn.Read(buffer)
+
+		if err != nil {
+			log.Println(conn.RemoteAddr().String(), " connection error: ", err)
+			return
+		}
+
+		log.Println(conn.RemoteAddr().String(), "receive data string:", string(buffer[:n]))
+	}
+}
+
+func CheckError(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error :%s", err.Error())
+		os.Exit(1)
+	}
 }
