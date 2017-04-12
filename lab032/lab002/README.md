d54a48094f89c7c533d2aa021a82732d98bb76de
liguoqinjim <liguoqinjim23@gmail.com>
2017-04-12 15:29:30 +0800

    lab032->lab002 简单压力测试

--- lab032/lab002/README.md
+++ lab032/lab002/README.md
@@ -1,7 +1,10 @@
 ###简单压力测试
 
 ####运行结果
+`go test -bench=.`不加-bench的时候不会运行BenchmarkXXX的方法。
+函数名后面跟的`-8`表示cpu的数量，`200000000`表示循环次数，`1.08 ns/op`表示平均每次执行时间
 
+![Imgur](http://i.imgur.com/lyUEW3b.png)
 
 ####参考资料
 https://github.com/astaxie/build-web-application-with-golang/blob/master/zh/11.3.md
--- lab032/lab002/lib_test.go
+++ lab032/lab002/lib_test.go
@@ -4,20 +4,18 @@ import (
 	"testing"
 )
 
-func BenchmarkLoops(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		Division(6, 7)
-	}
-}
 func BenchmarkDivision(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Division(6, 7)
+		Division(4, 5)
 	}
 }
-func TestDivision1(t *testing.T) {
-	if i, e := Division(6, 2); i != 3 || e != nil {
-		t.Error("除法函数测试没通过")
-	} else {
-		t.Log("第一个测试通过了")
+func BenchmarkTimeConsumingFunction(b *testing.B) {
+	b.StopTimer() //调用该函数停止压力测试的时间计数
+
+	//做一些初始化的工作,例如读取文件数据,数据库连接之类的,
+	//这样这些时间不影响我们测试函数本身的性能
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		Division(6, 7)
 	}
 }
