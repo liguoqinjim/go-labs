3aac1f9ae88a5db471996e316eeea186ae5eb4f9
liguoqinjim <liguoqinjim23@gmail.com>
2017-04-25 19:59:05 +0800

    lab046->lab002 最简单的服务器客户端收发数据

--- lab046/lab002/README.md
+++ lab046/lab002/README.md
@@ -1,1 +1,12 @@
 ### 简单实验2
+只是用snet写了一个简单的服务器和客户端，只测试了发送和返回的功能
+
+#### 运行结果
+##### client
+![Imgur](http://i.imgur.com/y5Jh1MI.png)
+
+##### server
+![Imgur](http://i.imgur.com/Gksfh1g.png)
+
+#### 参考资料
+https://github.com/funny/snet
---
+++ lab046/lab002/server/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"github.com/funny/snet/go"
+	"log"
+	"net"
+	"time"
+)
+
+func main() {
+	config := snet.Config{
+		EnableCrypt:        false,
+		HandshakeTimeout:   time.Second * 5,
+		RewriterBufferSize: 1024,
+		ReconnWaitTimeout:  time.Minute * 5,
+	}
+
+	listener, err := snet.Listen(config, func() (net.Listener, error) {
+		l, err := net.Listen("tcp", "0.0.0.0:8881")
+		checkErr(err)
+		return l, err
+	})
+	checkErr(err)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		handleConn(conn)
+	}
+}
+
+func handleConn(conn net.Conn) {
+	buffer := make([]byte, 2048)
+
+	for {
+		n, err := conn.Read(buffer)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		content := buffer[:n]
+		fmt.Println("服务器收到消息:content=", string(content))
+
+		//返回客户端
+		data := string(content) + "<-server"
+		conn.Write([]byte(data))
+	}
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Println(err)
+	}
+}
