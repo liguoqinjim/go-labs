ea33388868fad6549ff38ba4fe157ebad5a19858
liguoqinjim <liguoqinjim23@gmail.com>
2017-05-17 15:47:32 +0800

    lab056->lab002 format

--- lab056/lab002/main.go
+++ lab056/lab002/main.go
@@ -1,30 +1,31 @@
 package main
 
 import (
-	"github.com/go-redis/redis"
-	"lab056/lab002/conf"
 	"fmt"
+	"lab056/lab002/conf"
+
+	"github.com/go-redis/redis"
 )
 
 func main() {
 	conf.ReadConf()
 
 	client := redis.NewClient(&redis.Options{
-		Addr:conf.ConnConf.Addr,
+		Addr: conf.ConnConf.Addr,
 	})
 
-	Get := func(client *redis.Client,key string) *redis.StringCmd{
-		cmd := redis.NewStringCmd("get",key)
+	Get := func(client *redis.Client, key string) *redis.StringCmd {
+		cmd := redis.NewStringCmd("get", key)
 		client.Process(cmd)
 		return cmd
 	}
 
-	v,err := Get(client,"key2").Result()
-	if err == redis.Nil{
+	v, err := Get(client, "key2").Result()
+	if err == redis.Nil {
 		fmt.Println("key dose not exits")
-	}else if err != nil{
+	} else if err != nil {
 		panic(err)
-	}else{
-		fmt.Println("v",v)
+	} else {
+		fmt.Println("v", v)
 	}
 }
