c0167c0e7052576eb6c2e0525e23c75358faf40b
liguoqinjim <liguoqinjim23@gmail.com>
2017-05-19 10:27:03 +0800

    lab058->lab001

--- lab058/lab001/consumer/main.go
+++ lab058/lab001/consumer/main.go
@@ -1,1 +1,26 @@
-package consumer
+package main
+
+import (
+	"log"
+	"sync"
+
+	"github.com/nsqio/go-nsq"
+)
+
+func main() {
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+
+	config := nsq.NewConfig()
+	q, _ := nsq.NewConsumer("write_test", "ch", config)
+	q.AddHandler(nsq.HandlerFunc(func(message *nsq.Message) error {
+		log.Printf("Got a message: %v", message)
+		wg.Done()
+		return nil
+	}))
+	err := q.ConnectToNSQD("192.168.116.130:4150")
+	if err != nil {
+		log.Panic("Could not connect")
+	}
+	wg.Wait()
+}
--- lab058/lab001/producer/main.go
+++ lab058/lab001/producer/main.go
@@ -2,8 +2,18 @@ package main
 
 import (
 	"log"
+
+	"github.com/nsqio/go-nsq"
 )
 
 func main() {
+	config := nsq.NewConfig()
+	w, _ := nsq.NewProducer("192.168.116.130:4150", config)
 
+	err := w.Publish("write_test", []byte("test"))
+	if err != nil {
+		log.Panic("Could not connect")
+	}
+
+	w.Stop()
 }
