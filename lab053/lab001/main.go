e65432b1ecfcb85c8d56194091545c2fe19e87da
liguoqinjim <liguoqinjim23@gmail.com>
2017-06-03 16:54:55 +0800

    lab053->lab001 tutorial

--- lab053/lab001/main.go
+++ lab053/lab001/main.go
@@ -4,9 +4,17 @@ import (
 	"github.com/hoisie/web"
 )
 
-func hello(val string) string { return "hello " + val }
+func hello(val string) string {
+	return "hello " + val
+}
+
+func hi(ctx *web.Context, val string) {
+	ctx.WriteString("hi " + val)
+}
 
 func main() {
-	web.Get("/(.*)", hello)
+	web.Get("/hello(.*)", hello)
+	web.Get("/hi(.*)", hi)
+
 	web.Run("0.0.0.0:9999")
 }
