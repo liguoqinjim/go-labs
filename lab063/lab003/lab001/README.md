ac05d0937d4ba99d14a0fd977273bcd134658ae0
liguoqinjim <liguoqinjim23@gmail.com>
2017-06-13 14:28:42 +0800

    lab063->lab003->lab001 实验打开网页的响应速度

---
+++ lab063/lab003/lab001/README.md
@@ -0,0 +1,8 @@
+### 实验打开网页的响应速度
+就是运行了`cdp.Navigate`方法之后到打开网页，要多少时间。
+因为计算时间的方法不同，所以就只给出数据
+
+#### 运行结果
+![Imgur](http://i.imgur.com/aGmj63K.png)
+
+![Imgur](http://i.imgur.com/ftwh17y.png)
--- lab063/lab003/lab001/main.go
+++ lab063/lab003/lab001/main.go
@@ -5,7 +5,10 @@ import (
 	"log"
 
 	cdp "github.com/knq/chromedp"
+	cdptypes "github.com/knq/chromedp/cdp"
 	"github.com/knq/chromedp/client"
+	"os"
+	"time"
 )
 
 func main() {
@@ -22,17 +25,32 @@ func main() {
 	}
 
 	// run task list
-	var site, res string
 	err = c.Run(ctxt, baiduSearch())
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	log.Printf("saved screenshot of #testimonials from search result listing `%s` (%s)", res, site)
 }
 
 func baiduSearch() cdp.Tasks {
+	//file:///E:/Workspace/go-labs/src/lab063/lab003/lab001/testdata/time.html
+	pwd, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	filePath := "file:///" + pwd + "/testdata/time.html"
+
 	return cdp.Tasks{
-		cdp.Navigate(`https://www.baidu.com`),
+		cdp.ActionFunc(func(context.Context, cdptypes.Handler) error {
+			log.Println("time1", time.Now().Unix())
+			log.Println("time1", time.Now().UnixNano())
+			log.Println(filePath)
+			return nil
+		}),
+		cdp.Navigate(filePath),
+		cdp.ActionFunc(func(context.Context, cdptypes.Handler) error {
+			log.Println("time2", time.Now().Unix())
+			log.Println("time2", time.Now().UnixNano())
+			return nil
+		}),
 	}
 }
