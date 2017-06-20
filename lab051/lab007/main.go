c10d4bbb73576463b3cf15579757c38f6402ced7
liguoqinjim <liguoqinjim23@gmail.com>
2017-06-20 16:53:57 +0800

    lab051->lab007

---
+++ lab051/lab007/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	b3 "github.com/liguoqinjim/behavior3go"
+	//"github.com/liguoqinjim/behavior3go/config"
+	//"github.com/liguoqinjim/behavior3go/core"
+	//"github.com/liguoqinjim/behavior3go/loader"
+	"fmt"
+	"lab051/lab006/util"
+)
+
+type Army struct {
+	x      int
+	side   int //队伍的左右
+	attack int //攻击力
+	life   int //生命值
+	speed  int //移动速读
+}
+
+func main() {
+	a1 := &Army{0, util.SIDE_LEFT, 10, 100, 10}
+	a2 := &Army{100, util.SIDE_RIGHt, 5, 50, 5}
+	fmt.Println(a1, a2)
+
+	extMap := b3.NewRegisterStructMaps()
+}
---
+++ lab051/lab007/util/consts.go
@@ -0,0 +1,6 @@
+package util
+
+const (
+	SIDE_LEFT  = 1
+	SIDE_RIGHt = 2
+)
