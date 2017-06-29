e37275214486e49a8d6a70cda9d61188a1247e8b
liguoqinjim <liguoqinjim23@gmail.com>
2017-06-29 20:59:59 +0800

    lab058->lab004 完成

--- lab058/lab004/consumer/main.go
+++ lab058/lab004/consumer/main.go
@@ -1,14 +1,40 @@
 package main
 
 import (
+	"github.com/nsqio/go-nsq"
 	"io/ioutil"
 	"log"
 	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
 	ip := readConf()
-	log.Println("ip=", ip)
+	log.Println("nsq address=", ip)
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGTERM|syscall.SIGINT)
+
+	config := nsq.NewConfig()
+	q, err := nsq.NewConsumer("test004", "ch", config)
+	if err != nil {
+		log.Fatal("NewConsumer error:", err)
+	}
+	q.AddHandler(nsq.HandlerFunc(MsgHandler))
+
+	err = q.ConnectToNSQLookupd(ip)
+	if err != nil {
+		log.Fatal("ConnectToNSQLookupd error:", err)
+	}
+
+	<-sigs
+	log.Println("程序结束")
+}
+
+func MsgHandler(message *nsq.Message) error {
+	log.Println("收到消息:", string(message.ID[:]), string(message.Body))
+	return nil
 }
 
 func readConf() string {
--- lab058/lab004/producer/main.go
+++ lab058/lab004/producer/main.go
@@ -1,18 +1,35 @@
 package main
 
 import (
+	"fmt"
 	"github.com/nsqio/go-nsq"
 	"io/ioutil"
 	"log"
 	"os"
+	"time"
 )
 
 func main() {
-	config := nsq.NewConfig()
-	_ = config
-
 	address := readConf()
-	log.Println("address=", address)
+	log.Println("nsq address=", address)
+
+	config := nsq.NewConfig()
+	w, err := nsq.NewProducer(address, config)
+	if err != nil {
+		log.Fatal("NewProducer error:", err)
+	}
+	defer w.Stop()
+
+	for i := 0; i < 10; i++ {
+		message := fmt.Sprintf("hello %d", i)
+		err := w.Publish("test004", []byte(message))
+		if err != nil {
+			log.Fatal("Publish error:", err)
+		} else {
+			log.Println("发送消息:", message)
+		}
+		time.Sleep(time.Second * 2)
+	}
 }
 
 func readConf() string {
