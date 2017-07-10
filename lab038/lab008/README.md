b957b2d08dd1ce729b5a37143614d715a9df72bb
liguoqinjim <liguoqinjim23@gmail.com>
2017-07-10 14:30:02 +0800

    lab038->lab008 any

--- lab038/lab008/README.md
+++ lab038/lab008/README.md
@@ -1,4 +1,12 @@
 ### 测试any
+在proto中使用any
+
+#### 注意
+any的使用，还是要在`.proto`文件里面定义几个message的。比如，我定义了三个message，分别是A、B、C
+那么我可以在C里面创建一个any字段，我可以在any里面放A或者B属性，C都可以解析。是这样一个功能。
+
+#### 运行结果
+![Imgur](http://i.imgur.com/RII6UFB.png)
 
 #### 参考资料
 1. https://medium.com/@pokstad/sending-any-any-thing-in-golang-with-protobuf-3-95f84838028d
--- lab038/lab008/main.go
+++ lab038/lab008/main.go
@@ -4,42 +4,84 @@ import (
 	"fmt"
 	"github.com/golang/protobuf/proto"
 	"github.com/golang/protobuf/ptypes"
+	_ "github.com/golang/protobuf/ptypes"
+	"github.com/golang/protobuf/ptypes/any"
 	"io/ioutil"
 	"lab038/lab008/pb"
 	"log"
 )
 
-type ErrorInfo struct {
-}
-
-type Info struct {
-	InfoId int
-	Info   string
-}
-
-func (i *Info) Reset() {
-
-}
-func (i *Info) String() string {
-	return fmt.Sprintf("%d,%s", i.InfoId, i.Info)
-}
-func (i *Info) ProtoMessage() {
-
-}
-
 func main() {
 	es1 := &pb.ErrorStatus{Message: "error1"}
-	info1 := &Info{InfoId: 23, Info: "hello"}
-	any, err := ptypes.MarshalAny(info1)
-	if err != nil {
-		log.Fatalln("failed to marshalAny:", err)
-	}
-	es1.Details[0] = any
-	fmt.Println(es1)
+	es2 := &pb.ErrorStatus{Message: "error2"}
+	player := &pb.Player{PlayerId: 10001, PlayerName: "firstPlayer", PlayerGold: 2000}
+	playerHero := &pb.PlayerHero{Id: 90001, HeroId: 3, HeroLv: 1, HeroLife: 100}
 
-	//write to file
-	out1, err := proto.Marshal(es1)
-	if err := ioutil.WriteFile("tmp1.out", out1, 0644); err != nil {
-		log.Fatalln("failed to writeFile:", err)
+	//处理es1
+	data1, err := proto.Marshal(player)
+	if err != nil {
+		log.Fatalln("failed to marshal player:", err)
 	}
+	es1.Params = &any.Any{
+		TypeUrl: "/" + proto.MessageName(player),
+		Value:   data1,
+	}
+	//处理es2
+	data2, err := proto.Marshal(playerHero)
+	if err != nil {
+		log.Fatalln("failed to marshal playerHero:", err)
+	}
+	es2.Params = &any.Any{
+		TypeUrl: "/" + proto.MessageName(playerHero),
+		Value:   data2,
+	}
+
+	//写文件
+	out1, err := proto.Marshal(es1)
+	if err != nil {
+		log.Fatalln("failed to marshal es1:", err)
+	}
+	if err := ioutil.WriteFile("tmp1.out", out1, 0644); err != nil {
+		log.Fatalln("failed to write file1:", err)
+	}
+	out2, err := proto.Marshal(es2)
+	if err != nil {
+		log.Fatalln("failed to marshal es2:", err)
+	}
+	if err := ioutil.WriteFile("tmp2.out", out2, 0644); err != nil {
+		log.Fatalln("failed to write file2:", err)
+	}
+
+	//读文件
+	in1, err := ioutil.ReadFile("tmp1.out")
+	if err != nil {
+		log.Fatalln("failed to read file1:", err)
+	}
+	in2, err := ioutil.ReadFile("tmp2.out")
+	if err != nil {
+		log.Fatalln("failed to read file2:", err)
+	}
+
+	//parse
+	es11 := new(pb.ErrorStatus)
+	es22 := new(pb.ErrorStatus)
+	if err := proto.Unmarshal(in1, es11); err != nil {
+		log.Fatalln("failed to parse1:", err)
+	}
+	if err := proto.Unmarshal(in2, es22); err != nil {
+		log.Fatalln("failed to parse2:", err)
+	}
+	fmt.Println(es11)
+	fmt.Println(es22)
+	//parse any
+	player2 := new(pb.Player)
+	playerHero2 := new(pb.PlayerHero)
+	if err := ptypes.UnmarshalAny(es11.Params, player2); err != nil {
+		log.Fatalln("failed to parse any1:", err)
+	}
+	if err := ptypes.UnmarshalAny(es22.Params, playerHero2); err != nil {
+		log.Fatalln("failed to parse any2:", err)
+	}
+	fmt.Println("player2:", player2)
+	fmt.Println("playerHero2:", playerHero2)
 }
