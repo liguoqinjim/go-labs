d9ae1c58e8e62ba13f6b0527670a67f8ff3e8595
liguoqinjim <liguoqinjim23@gmail.com>
2017-07-14 14:16:56 +0800

    lab009->lab003 blob类型

---
+++ lab009/lab003/README.md
@@ -0,0 +1,4 @@
+### blob类型
+
+#### 运行结果
+![Imgur](http://i.imgur.com/pEAitHT.png)
--- lab009/lab003/main.go
+++ lab009/lab003/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 	"io/ioutil"
 	"log"
 	"os"
@@ -14,12 +16,6 @@ type DBConf struct {
 	DBPwd  string
 	DBName string
 }
-
-//
-//"DBHost": "DBHost",
-//"DBUser": "DBUser",
-//"DBPwd": "DBPwd",
-//"DBName": "DBName"
 
 var Conf *DBConf
 
@@ -49,5 +45,77 @@ func readConf() *DBConf {
 }
 
 func main() {
-	fmt.Println(Conf)
+	connectInfo := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?charset=utf8&parseTime=True&loc=Local", Conf.DBUser, Conf.DBPwd, Conf.DBHost, Conf.DBName)
+	db, err := sql.Open("mysql", connectInfo)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("数据库连接成功")
+
+	//insert blob类型
+	stmtIns, err := db.Prepare("insert into t_test_blob(mailData) value(?)")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer stmtIns.Close()
+	data := []byte("helloworld")
+	fmt.Println("data=", data)
+	_, err = stmtIns.Exec(data)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	//读
+	//rawbytes
+	fmt.Println()
+	rows, err := db.Query("select * from t_test_blob limit 2")
+	if err != nil {
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
+
+	// Get column names
+	columns, err := rows.Columns()
+	if err != nil {
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
+
+	// Make a slice for the values
+	values := make([]sql.RawBytes, len(columns))
+
+	//rows.Scan wants '[]interface{}' as an argument, so we must copy the
+	//references into such a slice
+	//See http://code.google.com/p/go-wiki/wiki/InterfaceSlice for details
+	scanArgs := make([]interface{}, len(values))
+	for i := range values {
+		scanArgs[i] = &values[i]
+	}
+
+	// Fetch rows
+	for rows.Next() {
+		// get RawBytes from data
+		err = rows.Scan(scanArgs...)
+		if err != nil {
+			panic(err.Error()) // proper error handling instead of panic in your app
+		}
+
+		var value string
+		for i, col := range values {
+			if col == nil {
+				value = "NULL"
+			} else {
+				value = string(col)
+			}
+			fmt.Println(columns[i], ": ", value)
+			fmt.Println("receive data:", col)
+		}
+	}
+	if err = rows.Err(); err != nil {
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
 }
