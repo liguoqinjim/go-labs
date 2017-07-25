c466fb8df55f4798892997ed0e72a41b2aa48025
liguoqinjim <liguoqinjim23@gmail.com>
2017-07-25 17:43:48 +0800

    lab075->lab003

--- lab075/lab003/README.md
+++ lab075/lab003/README.md
@@ -1,7 +1,10 @@
 ### for和select
 
+#### 注意
+select里面的break只是跳出select，但是select里面的continue还是直接到下一次的for循环
+
 #### 运行结果
-![Imgur](http://i.imgur.com/BNFYgt4.png)
+![Imgur](http://i.imgur.com/1rkAMKt.png)
 
 #### 参考资料
 http://blog.csdn.net/ZongYinHu/article/details/54695434
--- lab075/lab003/main.go
+++ lab075/lab003/main.go
@@ -14,26 +14,6 @@ type Army struct {
 	ReturnSig chan int
 }
 
-func (a *Army) Move() {
-	log.Printf("Army[%d] start move\n", a.Aid)
-	for {
-		select {
-		case <-a.ReturnSig:
-			log.Printf("Army[%d] Return!\n", a.Aid)
-		default:
-			a.Apos += 10
-			log.Printf("Army[%d].Apos=%d\n", a.Aid, a.Apos)
-			time.Sleep(time.Second * 2)
-			continue
-		}
-		break
-	}
-}
-
-func (a *Army) Stop() {
-	log.Printf("Army[%d] stop!\n", a.Aid)
-}
-
 func main() {
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
@@ -46,4 +26,26 @@ func main() {
 
 	<-sigs
 	log.Println("end")
+}
+
+func (a *Army) Move() {
+	log.Printf("Army[%d] start move\n", a.Aid)
+	for {
+		log.Println("Move的for循环")
+		select {
+		case <-a.ReturnSig:
+			log.Printf("Army[%d] Return!\n", a.Aid)
+		default:
+			a.Apos += 10
+			log.Printf("Army[%d].Apos=%d\n", a.Aid, a.Apos)
+			time.Sleep(time.Second * 2)
+			continue
+		}
+		break
+	}
+	log.Printf("Army[%d] Move() quit\n", a.Aid)
+}
+
+func (a *Army) Stop() {
+	log.Printf("Army[%d] stop!\n", a.Aid)
 }
