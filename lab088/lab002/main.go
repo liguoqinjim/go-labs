13f720dd6dce84068a3b09894778c7f1a3c44842
liguoqinjim <liguoqinjim23@gmail.com>
2017-09-27 22:07:43 +0800

    lab088->lab002

---
+++ lab088/lab002/main.go
@@ -0,0 +1,18 @@
+package main
+
+type Point struct {
+	X, Y float64
+}
+
+type Line struct {
+	P1, P2 *Point
+	K, B   float64
+}
+
+func NewLine(p1, p2 *Point) *Line {
+	return nil
+}
+
+func main() {
+
+}
