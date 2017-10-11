7e03c26987c732453b890f264312de6819c21f28
liguoqinjim <liguoqinjim23@gmail.com>
2017-10-11 15:00:44 +0800

    lab024->lab001 github.com/tealeg/xlsx 读取xlsx

--- lab024/READMD.md
+++ lab024/READMD.md
@@ -2,5 +2,5 @@
 
 |实验|简介|
 |---|---|
-|lab001|示例程序01，github.com/tealeg/xlsx|
+|lab001|示例程序01，github.com/tealeg/xlsx，读取xlsx|
 |lab002|示例程序02，github.com/tealeg/xlsx|
--- lab024/lab001/main.go
+++ lab024/lab001/main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/tealeg/xlsx"
+	"log"
+)
 
+func main() {
+	excelFileName := "testFile.xlsx"
+	xlFile, err := xlsx.OpenFile(excelFileName)
+	if err != nil {
+		log.Println("OpenFile error:", err)
+	}
+
+	for _, sheet := range xlFile.Sheets {
+		for _, row := range sheet.Rows {
+			for _, cell := range row.Cells {
+				text := cell.String()
+				fmt.Printf("%s\t", text)
+			}
+			fmt.Println()
+		}
+	}
 }
