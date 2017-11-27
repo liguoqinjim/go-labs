f9420d887e831d002baff314c6f34023441da2ba
liguoqinjim <liguoqinjim23@gmail.com>
2017-11-27 10:44:20 +0800

    lab004

--- lab093/lab004/README.md
+++ lab093/lab004/README.md
@@ -1,4 +1,4 @@
-### 搜索文件夹
+### 遍历文件夹
 
 #### 参考资料
 https://stackoverflow.com/questions/6608873/file-system-scanning-in-golang
--- lab093/lab004/main.go
+++ lab093/lab004/main.go
@@ -2,34 +2,19 @@ package main
 
 import (
 	"flag"
-	"github.com/henrylee2cn/faygo/errors"
-	"log"
+	"fmt"
 	"os"
 	"path/filepath"
-	"strings"
 )
 
-var filterPath = "dir2"
-
 func visit(path string, f os.FileInfo, err error) error {
-	log.Println("Visited:", path, "--------------------------------------------------------")
-	log.Println("是否是文件夹", f.IsDir())
-
-	if strings.Contains(path, filterPath) {
-		return errors.New(path)
-	} else {
-		return nil
-	}
+	fmt.Printf("Visited: %s\n", path)
+	return nil
 }
 
 func main() {
 	flag.Parse()
 	root := flag.Arg(0)
 	err := filepath.Walk(root, visit)
-
-	if err == nil {
-		log.Println("没有找到文件夹")
-	} else {
-		log.Println("找到文件夹:", err.Error())
-	}
+	fmt.Printf("filepath.Walk() returned %v\n", err)
 }
