4b837a9b9e6ede20363a9a1544e0c423344a0969
liguoqinjim <liguoqinjim23@gmail.com>
2017-12-13 14:40:22 +0800

    lab045->lab001

---
+++ lab045/lab001/README.md
@@ -0,0 +1,7 @@
+### example01
+
+#### 运行结果
+![Imgur](https://i.imgur.com/tH8rlEc.png)
+
+#### 参考资料
+https://gist.github.com/drewolson/4771479
---
+++ lab045/lab001/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"reflect"
+)
+
+type Foo struct {
+	FirstName string `tag_name:"tag 1"`
+	LastName  string `tag_name:"tag 2"`
+	Age       int    `tag_name:"tag 3"`
+}
+
+func (f *Foo) reflect() {
+	val := reflect.ValueOf(f).Elem()
+
+	for i := 0; i < val.NumField(); i++ {
+		valueField := val.Field(i)
+		typeField := val.Type().Field(i)
+		tag := typeField.Tag
+
+		log.Printf("Field Name: %s,\t Field Value: %v,\t Tag Value: %s\n", typeField.Name, valueField.Interface(), tag.Get("tag_name"))
+	}
+}
+
+func main() {
+	f := &Foo{
+		FirstName: "Drew",
+		LastName:  "Olson",
+		Age:       30,
+	}
+
+	f.reflect()
+}
