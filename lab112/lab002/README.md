07adce6b55e01b3290e8f0d7d175f571201ea1c0
liguoqinjim <liguoqinjim23@gmail.com>
2018-01-04 11:29:14 +0800

    lab002 删除cookie

--- lab112/lab002/README.md
+++ lab112/lab002/README.md
@@ -1,2 +1,6 @@
-###
+### 删除cookie
+设置maxAge为负数
+
+#### 参考资料
+https://studygolang.com/articles/5905
 
--- lab112/lab002/main.go
+++ lab112/lab002/main.go
@@ -13,12 +13,20 @@ func setCookieHandle(w http.ResponseWriter, req *http.Request) {
 	http.SetCookie(w, &c2)
 }
 
-func delCookieHandle(w http.ResponseWriter,req *http.Request){
-	
+func delCookieHandle(w http.ResponseWriter, req *http.Request) {
+	c, err := req.Cookie("key1")
+	if err != nil {
+		log.Printf("req.Cookie error:%v", err)
+		return
+	}
+
+	c.MaxAge = -1
+	http.SetCookie(w, c)
 }
 
 func main() {
 	http.HandleFunc("/setCookie", setCookieHandle)
+	http.HandleFunc("/delCookie", delCookieHandle)
 	log.Println("Listening...")
 
 	http.ListenAndServe("127.0.0.1:9999", nil)
