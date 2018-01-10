0049c6ad4a1495d5cd1a08099fe44b7d6c7ef0e8
liguoqinjim <liguoqinjim23@gmail.com>
2018-01-10 10:53:45 +0800

    lab001

--- lab104/lab001/main.go
+++ lab104/lab001/main.go
@@ -6,7 +6,10 @@ import (
 )
 
 func main() {
-	u1 := uuid.NewV4()
+	u1, err := uuid.NewV4()
+	if err != nil {
+		log.Fatalf("uuid.NewV4 error:%v", err)
+	}
 	log.Printf("UUIDv4: %s\n", u1)
 
 	u2, err := uuid.FromString("6ba7b810-9dad-11d1-80b4-00c04fd430c8")
