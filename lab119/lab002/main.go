70b2e78a4bd82642c84a839dfc24df4aea147390
liguoqinjim <liguoqinjim23@gmail.com>
2018-01-20 15:13:59 +0800

    lab002 gb2312<->utf8

---
+++ lab119/lab002/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bytes"
+	"golang.org/x/text/encoding/simplifiedchinese"
+	"golang.org/x/text/transform"
+	"io/ioutil"
+	"log"
+)
+
+func GB2312ToUTF8(s []byte) ([]byte, error) {
+	reader := transform.NewReader(bytes.NewReader(s), simplifiedchinese.HZGB2312.NewDecoder())
+	d, e := ioutil.ReadAll(reader)
+	if e != nil {
+		return nil, e
+	}
+
+	return d, nil
+}
+
+func UTF8ToGB2312(s []byte) ([]byte, error) {
+	reader := transform.NewReader(bytes.NewReader(s), simplifiedchinese.HZGB2312.NewEncoder())
+	d, e := ioutil.ReadAll(reader)
+	if e != nil {
+		return nil, e
+	}
+	return d, nil
+}
+
+func main() {
+	s := "GB2312 与 UTF-8 编码转换测试"
+
+	gb2312, err := UTF8ToGB2312([]byte(s))
+	if err != nil {
+		log.Printf("UTF8ToGB2312 error:%v", err)
+	} else {
+		log.Println("gb2312=", string(gb2312))
+	}
+
+	utf8, err := GB2312ToUTF8(gb2312)
+	if err != nil {
+		log.Printf("GB2312ToUTF8 error:%v", err)
+	} else {
+		log.Println("utf8=", string(utf8))
+	}
+}
