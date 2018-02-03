8a12162844f75cd36677aa6169f517e943fe9827
liguoqinjim <liguoqinjim23@gmail.com>
2018-02-03 10:48:06 +0800

    lab004

--- lab123/README.md
+++ lab123/README.md
@@ -6,3 +6,4 @@
 |lab001|example_phantomjs|
 |lab002|example_chrome|
 |lab003|example_firefox(暂时不能用)|
+|lab004|chrome模拟点击|
--- lab123/lab004/main.go
+++ lab123/lab004/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/sclevine/agouti"
 	"log"
+	"time"
 )
 
 func main() {
@@ -10,17 +11,26 @@ func main() {
 	if err := driver.Start(); err != nil {
 		log.Fatalf("Failed to start driver:%v", err)
 	}
-	defer driver.Stop()
+	defer func() {
+		time.Sleep(time.Second * 10)
+		driver.Stop()
+	}()
 
 	page, err := driver.NewPage(agouti.Browser("chrome"))
 	if err != nil {
 		log.Fatalf("Failed to open page:%v", err)
 	}
 
+	//打开页面
 	if err := page.Navigate("https://www.baidu.com/"); err != nil {
 		log.Fatalf("Failed to navigate:%v", err)
 	}
-	if err := page.Screenshot("E:/Workspace/go-labs/src/lab123/lab002/tmp/chrome_baidu.jpg"); err != nil {
-		log.Fatalf("Failed to screenshot:%v", err)
+	//输入搜索关键字
+	if err := page.FindByID("kw").Fill("hello"); err != nil {
+		log.Printf("Failed to fill input:%v", err)
 	}
+
+	//if err := page.Screenshot("E:/Workspace/go-labs/src/lab123/lab004/tmp/chrome_baidu.jpg"); err != nil {
+	//	log.Fatalf("Failed to screenshot:%v", err)
+	//}
 }
