b56ef9b2c56269f0d7dbe8078621c34b4339a972
liguoqinjim <liguoqinjim23@gmail.com>
2018-03-30 15:44:57 +0800

    lab005 select

---
+++ lab128/lab005/README.md
@@ -0,0 +1,12 @@
+### select
+
+#### 注意点
+ - First和Find，First会返回第一个满足条件的结果，Find会返回所有满足条件的结果。所以要注意一下传入的参数
+ - Last会根据id排序
+ - 可以使用where来自定义select的条件，结合First和Find使用
+
+#### 运行结果
+![Imgur](https://i.imgur.com/UUifAsg.png)
+
+#### 参考资料
+ - http://gorm.io/docs/query.html
--- lab128/lab005/main.go
+++ lab128/lab005/main.go
@@ -40,21 +40,76 @@ func main() {
 	}
 	defer db.Close()
 
+	//判断是否有表
+	hasTable := db.HasTable("user_lab005")
+	if hasTable {
+		db.Debug().DropTable("user_lab005")
+	}
+
 	db.Debug().AutoMigrate(&User{})
 
-	//insert :Create
-	u1 := &User{Name: "tom", Age: 12}
-	db.Debug().Create(u1)
-	log.Printf("insert:u1=%+v", u1)
+	//insert data
+	u1 := &User{Name: "Tom", Age: 12}
+	u2 := &User{Name: "Kimi", Age: 13}
+	u3 := &User{Name: "Alice", Age: 15}
+	u4 := &User{Name: "Ben", Age: 17}
+	u5 := &User{Name: "Mark", Age: 18}
+	u6 := &User{Name: "Tom", Age: 1}
+	db.Create(u1)
+	db.Create(u2)
+	db.Create(u3)
+	db.Create(u4)
+	db.Create(u5)
+	db.Create(u6)
 
-	//insert :NewRecord
-	u2 := &User{Name: "kimi", Age: 13}
-	result := db.Debug().NewRecord(u2)
-	log.Printf("newRecord:u2=%+v,result=%t", u2, result)
+	//select
+	//First
+	var user User
+	db.Debug().First(&user)
+	log.Printf("db.First user:%+v", user)
+
+	//注意：这里在user上再使用Last，select语句里面的id会是user的id，所以结果不是最后一个id的对应的值
+	db.Debug().Last(&user)
+	log.Printf("db.Last user:%+v", user)
+
+	//Last
+	var user2 User
+	db.Debug().Last(&user2)
+	log.Printf("db.Last user2:%+v", user2)
+
+	//Find all
+	var users []User
+	db.Debug().Find(&users)
+	log.Printf("db.Find users:%+v", users)
+
+	//where plain sql
+	//这里用first，就会返回第一个match where条件的值
+	var user4 User
+	db.Debug().Where("name = ?", "Tom").First(&user4)
+	log.Printf("where name=? First user4:%+v", user4)
+
+	//查找多个用Find
+	var users2 []User
+	db.Debug().Where("name = ?", "Tom").Find(&users2)
+	log.Printf("where name=? Find users2:%+v", users2)
+
+	//多个条件
+	var user5 User
+	db.Debug().Where("name = ? and age = ?", "Tom", 1).First(&user5)
+	log.Printf("where name=? and age=? First user5:%+v", user5)
+
+	//where in的条件
+	var users3 []User
+	db.Debug().Where("name in (?)", []string{"Kimi", "Alice"}).Find(&users3)
+	log.Printf("where name in (?) Find users3:%+v", &users3)
 }
 
 type User struct {
 	Id   int `gorm:"auto_increment"`
 	Name string
 	Age  int
+}
+
+func (User) TableName() string {
+	return "user_lab005"
 }
