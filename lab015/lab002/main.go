00718ef8ff3c5cfdbc9635020f9fd04ea4136d9f
liguoqinjim <liguoqinjim23@gmail.com>
2018-04-13 17:13:54 +0800

    lab002

--- lab015/lab002/main.go
+++ lab015/lab002/main.go
@@ -9,7 +9,7 @@ func main() {
 	//node E:\Workspace\js-labs\lab020\lab001\js\test1.js
 	//E:\\Workspace\\js-labs\\lab020\\lab002\\test1.js
 	//E:\Workspace\js-labs\lab022
-	nodejsCmd := exec.Command("node", "E:\\Workspace\\js-labs\\lab022\\test.js", `{"rid":"R_SO_4_186016","offset":"0","total":"false","limit":"20","csrf_token":""}`)
+	nodejsCmd := exec.Command("node", "E:\\Workspace\\js-labs\\lab022\\test.js", `'{"rid":"R_SO_4_186016","offset":"0","total":"false","limit":"20","csrf_token":""}'`)
 	goEnvOut, err := nodejsCmd.Output()
 	if err != nil {
 		log.Fatalf("output error:%v", err)
