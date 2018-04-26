2b438e12db570b38fc7ac26388471f3c8f7e4bfa
liguoqinjim <liguoqinjim23@gmail.com>
2018-04-26 15:47:21 +0800

    lab003

--- lab134/lab003/main.go
+++ lab134/lab003/main.go
@@ -1,5 +1,44 @@
 package main
 
-func main() {
+import (
+	"github.com/samuel/go-zookeeper/zk"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+	"time"
+)
 
+func main() {
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	//读取数据，用|隔开
+	conf, err := ioutil.ReadFile("zk.conf")
+	if err != nil {
+		log.Fatalf("readFile error:%v", err)
+	}
+
+	conns := strings.Split(string(conf), "|")
+	conn, ech, err := zk.Connect(conns, time.Second)
+	if err != nil {
+		log.Fatalf("zk.Connect error:%v", err)
+	}
+	go func() {
+		for {
+			select {
+			case e := <-ech:
+				log.Printf("get Event :%+v", e)
+			}
+		}
+	}()
+
+	//stat
+	s := conn.State()
+	log.Printf("stat=%+v", s)
+
+	<-sigs
+	log.Println("program end")
 }
