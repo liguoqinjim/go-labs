abe873e5c078a7ae3d91050e6e0a40969c66a6d0
liguoqinjim <liguoqinjim23@gmail.com>
2018-04-26 18:08:50 +0800

    lab005

---
+++ lab134/lab005/README.md
@@ -0,0 +1,4 @@
+###
+
+#### 参考资料
+ - https://holynull.gitbooks.io/zookeeper/content/pei_zhi_fu_wu_configuration_service.html
---
+++ lab134/lab005/master/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"github.com/samuel/go-zookeeper/zk"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+	"time"
+)
+
+func main() {
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	//读取数据，用|隔开
+	conf, err := ioutil.ReadFile("../zk.conf")
+	if err != nil {
+		log.Fatalf("readFile error:%v", err)
+	}
+
+	conns := strings.Split(string(conf), "|")
+	c, ech, err := zk.Connect(conns, time.Second)
+	if err != nil {
+		log.Fatalf("zk.Connect error:%v", err)
+	}
+	go func() {
+		for {
+			select {
+			case e := <-ech:
+				log.Printf("get Event :%+v", e)
+			}
+		}
+	}()
+
+	path := "/" + "lab005"
+	r, err := c.Create(path, []byte("hello"), zk.FlagEphemeral, zk.WorldACL(zk.PermAll))
+	if err != nil {
+		log.Fatalf("Create error:%v", err)
+	} else {
+		log.Println("r=", r)
+	}
+
+	<-sigs
+	log.Println("master program end")
+}
