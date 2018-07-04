283eeb2e1d7fb59662d58d0349eb4441d8209fc8
liguoqinjim <liguoqinjim23@gmail.com>
2018-07-04 15:26:11 +0800

    lab003

---
+++ lab143/lab003/README.md
@@ -0,0 +1,4 @@
+### gorequest
+
+#### 运行结果
+![Imgur](https://i.imgur.com/GrOY84e.png)
--- lab143/lab003/main.go
+++ lab143/lab003/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/parnurzeal/gorequest"
 	"io/ioutil"
 	"log"
 )
@@ -35,28 +36,25 @@ func main() {
 	u := fmt.Sprintf(conf.UrlFormat, conf.EventName, conf.Key)
 	log.Printf("url=%s", u)
 
-	//client
-	//client := &http.Client{}
-	//values := map[string]string{
-	//	"value1": conf.Value1,
-	//	"value2": conf.Value2,
-	//	"value3": conf.Value3,
-	//}
-	//jsonStr, _ := json.Marshal(values)
-	//
-	//req, err := http.NewRequest(http.MethodPost, u, bytes.NewBuffer(jsonStr))
-	//if err != nil {
-	//	log.Fatalf("http.NewRequest error:%v", err)
-	//}
-	//req.Header.Add("Content-Type", "application/json")
-	//
-	//resp, err := client.Do(req)
-	//if err != nil {
-	//	log.Fatalf("client.Do error:%v", err)
-	//}
-	//d, err := ioutil.ReadAll(resp.Body)
-	//if err != nil {
-	//	log.Fatalf("ioutil.ReadAll error:%v", err)
-	//}
-	//log.Printf("resp:\n%s", d)
+	type ValueStruct struct {
+		Value1 string `json:"value1"`
+		Value2 string `json:"value2"`
+		Value3 string `json:"value3"`
+	}
+	val := ValueStruct{Value1: conf.Value1, Value2: conf.Value2, Value3: conf.Value3}
+	request := gorequest.New()
+	//http://httpbin.org/post
+	resp, body, errs := request.Post(u).
+		Send(val).
+		End()
+	if errs != nil {
+		log.Fatalf("errs:%+v", errs)
+	}
+	d, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("ioutil.ReadAll error:%v", err)
+	}
+	log.Printf("resp=\n%s", d)
+	log.Println("body.length=", len(body))
+	log.Println("body=", body)
 }
