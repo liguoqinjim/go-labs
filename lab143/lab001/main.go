d8ff274ff153281cc30ab0e7558c392be6beb5f4
liguoqinjim <liguoqinjim23@gmail.com>
2018-07-04 11:03:43 +0800

    lab001

--- lab143/lab001/main.go
+++ lab143/lab001/main.go
@@ -1,15 +1,25 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
+	"net/http"
+	"net/url"
 )
 
 type ConfStruct struct {
-	Key string `json:"key"`
+	Key       string `json:"key"`
+	EventName string `json:"event_name"`
+	UrlFormat string `json:"url_format"`
+	Value1    string `json:"value1"`
+	Value2    string `json:"value2"`
+	Value3    string `json:"value3"`
 }
 
-var Conf *ConfStruct
+var conf *ConfStruct
 
 func main() {
 	//读取配置文件
@@ -19,8 +29,29 @@ func main() {
 	}
 	log.Printf("data=\n%s", data)
 
-	//拼接url
+	err = json.Unmarshal(data, conf)
+	if err != nil {
+		log.Fatalf("json.Unmarshal error:%v", err)
+	}
 
+	//拼接url
+	u := fmt.Sprintf(conf.UrlFormat, conf.EventName, conf.Key)
+
+	form := url.Values{
+		"value1": {conf.Value1},
+		"value2": {conf.Value2},
+		"value3": {conf.Value3},
+	}
+	b := bytes.NewBufferString(form.Encode())
+	resp, err := http.Post(u, "application/json", b)
+	if err != nil {
+		log.Fatalf("http.Post error:%v", err)
+	}
+	d, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("ioutil.ReadAll error:%v", err)
+	}
+	log.Printf("resp:\n%s", d)
 
 	//在上面的post基础上指定contentType和body
 	//form := url.Values{
