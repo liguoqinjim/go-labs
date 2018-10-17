05904873c72e4b87d6819ededec2ba5918d5cb37
liguoqinjim <liguoqinjim23@gmail.com>
2018-10-17 17:30:19 +0800

    lab131

--- lab131/lab001/main.go
+++ lab131/lab001/main.go
@@ -12,4 +12,7 @@ func main() {
 
 	a := "opA%2BQIxz40y7oK%2Fdr%2BCGM%2F4VtSefmfPBe9gR%2BUpZ8nWEawWQQYSMrHgRy4M1Kk55S7iNjQcf4cNw0pLjEP4%2B54LW4PxwN%2Brpk14YawAFcihyCcJRNA0QXZRT8mnNTx9xDzbDUJHe1siqPmIQQ97vpL2y4S5Shm5sR4JuTYvNfeI2MiAhlHujmrjjE4LY1S%2Fy"
 	fmt.Println(url.QueryUnescape(a))
+
+	s := "上海"
+	fmt.Println(url.QueryEscape(s))
 }
