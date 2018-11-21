3be8cc83ede0364eab9f95945e4c15baccb17949
liguoqinjim <liguoqinjim23@gmail.com>
2018-11-21 20:27:43 +0800

    update

--- lab166/lab001/main.go
+++ lab166/lab001/main.go
@@ -1,18 +1,24 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/bculberson/bloom"
 	"github.com/garyburd/redigo/redis"
+	"io/ioutil"
+	"log"
 	"time"
 )
 
 func main() {
+	example()
+}
+
+func example() {
 	pool := &redis.Pool{
 		MaxIdle:     3,
 		IdleTimeout: 240 * time.Second,
-		Dial:        func() (redis.Conn, error) { return redis.Dial("tcp", addr) },
+		Dial:        func() (redis.Conn, error) { return redis.Dial("tcp", readAddr()) },
 	}
-
 
 	conn := pool.Get()
 	m, k := bloom.EstimateParameters(1000, .01)
@@ -20,5 +26,34 @@ func main() {
 	b := bloom.New(m, k, bitSet)
 	b.Add([]byte("some key"))
 	exists, _ := b.Exists([]byte("some key"))
+	log.Println(exists)
 	doesNotExist, _ := b.Exists([]byte("some other key"))
+	log.Println(doesNotExist)
+
+	b.Add([]byte("helloworld"))
+	log.Println(b.Exists([]byte("helloworld")))
+
+	b.Add([]byte("helloworld1"))
+	log.Println(b.Exists([]byte("helloworld1")))
+
+	b.Add([]byte("iris"))
+	log.Println(b.Exists([]byte("iris")))
+}
+
+func readAddr() string {
+	data, err := ioutil.ReadFile("../conf.json")
+	if err != nil {
+		log.Fatalf("ioutil.ReadFile error:%v", err)
+	}
+
+	var conf = &Conf{}
+	if err := json.Unmarshal(data, conf); err != nil {
+		log.Fatalf("json.Unmarshal error:%v", err)
+	}
+
+	return conf.Addr
+}
+
+type Conf struct {
+	Addr string `json:"addr"`
 }
