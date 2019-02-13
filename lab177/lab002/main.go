65d2abb8d3420dd87bffa58c15615e6a8e591a87
liguoqinjim <liguoqinjim23@gmail.com>
2019-02-13 17:01:57 +0800

    lab002 path exists

--- lab177/lab002/main.go
+++ lab177/lab002/main.go
@@ -1,11 +1,30 @@
 package main
 
-import "os"
+import (
+	"log"
+	"os"
+)
 
 func main() {
 	//注意：newpath也要跟着文件名
+
+	if ok, _ := exists("../data/3/"); !ok {
+		log.Fatalf("路径不存在")
+	}
+
 	err := os.Rename("../data/1/1.txt", "../data/3/1.txt")
 	if err != nil {
 		panic(err)
 	}
+}
+
+func exists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return true, err
 }
