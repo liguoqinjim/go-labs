6b7c72e5326c2065627430661ac1eae658fc9b7e
liguoqinjim <liguoqinjim23@gmail.com>
2019-03-07 12:51:34 +0800

    update multi output

--- lab183/lab001/main.go
+++ lab183/lab001/main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"github.com/natefinch/lumberjack"
+	"io"
+	"log"
+	"os"
+)
+
 func main() {
-	
+	log.SetOutput(io.MultiWriter(os.Stdout, &lumberjack.Logger{
+		Filename:   "foo.log",
+		MaxSize:    1,
+		MaxBackups: 3,
+		MaxAge:     28,
+		Compress:   false,
+	}))
+
+	for i := 0; i < 2000; i++ {
+		log.Println("hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world ")
+		log.Println("hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world ")
+		log.Println("hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world ")
+		log.Println("hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world ")
+		log.Println("hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world ")
+	}
 }
