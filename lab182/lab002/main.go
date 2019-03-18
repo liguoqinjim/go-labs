444412b4fc79f58defe23312698ce503ef2bf84b
liguoqinjim <liguoqinjim23@gmail.com>
2019-03-18 20:04:22 +0800

    update

--- lab182/lab002/main.go
+++ lab182/lab002/main.go
@@ -7,19 +7,21 @@ import (
 )
 
 func main() {
-	for i := 211; i <= 221; i++ {
+	//for i := 211; i <= 221; i++ {
 
-	}
+	for i := 17; i <= 20; i++ {
+		host := fmt.Sprintf("15.17.26.%d", i)
 
-	ps := portscanner.NewPortScanner("hp-111", 2*time.Second, 5)
+		ps := portscanner.NewPortScanner(host, 2*time.Second, 5)
 
-	fmt.Printf("scanning port %d-%d...\n", 20, 65535)
+		fmt.Printf("scanning %s port %d-%d...\n", host, 7400, 7600)
 
-	openedPorts := ps.GetOpenedPort(20, 30000)
+		openedPorts := ps.GetOpenedPort(20, 65535)
 
-	for i := 0; i < len(openedPorts); i++ {
-		port := openedPorts[i]
-		fmt.Print(" ", port, " [open]")
-		fmt.Println("  -->  ", ps.DescribePort(port))
+		for i := 0; i < len(openedPorts); i++ {
+			port := openedPorts[i]
+			fmt.Print(" ", port, " [open]")
+			fmt.Println("  -->  ", ps.DescribePort(port))
+		}
 	}
 }
