9390d9087388f2bf3d69495a6571b879389e8afc
liguoqinjim <liguoqinjim23@gmail.com>
2019-06-28 16:17:07 +0800

    update

--- lab188/main.go
+++ lab188/main.go
@@ -6,13 +6,13 @@ import (
 )
 
 func main() {
-	//db := ouidb.New("oui.txt")
-	db := ouidb.New("oui2.txt")
+	db := ouidb.New("oui_database.txt")
+	//db := ouidb.New("oui2.txt")
 	if db == nil {
 		log.Fatalf("db is nil")
 	}
 
-	mac := "38-29-5A-28-E3-8F" //D0-C7-C0-9A-EC-C8
+	mac := "D0-C7-C0-9A-EC-C8" //D0-C7-C0-9A-EC-C8
 	v, err := db.VendorLookup(mac)
 	if err != nil {
 		log.Fatalf("parse: %s: %s", mac, err.Error())
