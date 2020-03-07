32a182c4fe77c85f8a4c641d3f9d71e6a5e4af59
liguoqinjim <liguoqinjim23@gmail.com>
2020-03-07 22:18:30 +0800

    lab209 oss

--- lab209/lab001/main.go
+++ lab209/lab001/main.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"fmt"
+	"github.com/aliyun/aliyun-oss-go-sdk/oss"
 	"github.com/spf13/viper"
 	"os"
+	"strings"
 )
 
 var (
 	endpoint        string
 	accessKeyId     string
 	accessKeySecret string
+	bucketName      string
 )
 
 func init() {
@@ -17,13 +20,39 @@ func init() {
 }
 
 func main() {
+	client, err := oss.New(endpoint, accessKeyId, accessKeySecret)
+	if err != nil {
+		panic(err)
+	}
 
+	//list bucket
+	lsRes, err := client.ListBuckets()
+	if err != nil {
+		panic(err)
+	}
+	for _, bucket := range lsRes.Buckets {
+		fmt.Println("Buckets:", bucket.Name)
+
+		if strings.Contains(bucket.Name, "01") {
+			bucketName = bucket.Name
+		}
+	}
+
+	//put object
+	bucket, err := client.Bucket(bucketName)
+	if err != nil {
+		panic(err)
+	}
+	//名字一样的话，会被覆盖
+	if err := bucket.PutObjectFromFile("test001", "../data/1.png"); err != nil {
+		panic(err)
+	}
 }
 
 func readConfig() {
 	v := viper.New()
 	v.SetConfigName("config")
-	v.AddConfigPath(".")
+	v.AddConfigPath("..")
 	v.SetConfigType("yaml")
 
 	if err := v.ReadInConfig(); err != nil {
