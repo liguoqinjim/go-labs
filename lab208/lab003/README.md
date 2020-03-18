e201394308249bd742cfcd58ec019d5971ddb7b6
liguoqinjim <liguoqinjim23@gmail.com>
2020-03-18 10:47:24 +0800

    update

---
+++ lab208/lab003/README.md
@@ -0,0 +1,4 @@
+# 环境变量
+
+## 参考资料
+ - https://github.com/spf13/viper/issues/85
---
+++ lab208/lab003/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"github.com/spf13/viper"
+	"log"
+)
+
+func main() {
+	viper.AutomaticEnv()
+	log.Println(viper.Get("http_proxy"))
+}
