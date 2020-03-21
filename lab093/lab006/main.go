b4c8eb985268d32c412195952298d940d395cdab
liguoqinjim <liguoqinjim23@gmail.com>
2020-03-21 14:08:30 +0800

    lab093

--- lab093/lab006/main.go
+++ lab093/lab006/main.go
@@ -3,15 +3,20 @@ package main
 import (
 	"log"
 	"os"
+	"syscall"
 )
 
 func main() {
 	//os.Remove不可以删除
-	//if err := os.Remove("data/1"); err != nil {
-	//	log.Fatalf("os.Remove error:%v", err)
-	//}
-
-	if err := os.RemoveAll("data/1"); err != nil {
-		log.Fatalf("os.RemoveAll error:%v", err)
+	if err := os.Remove("data/1"); err != nil {
+		if err.(*os.PathError).Err == syscall.ENOTEMPTY {
+			log.Println("文件夹不为空")
+		} else {
+			log.Fatalf("os.Remove error:%v", err)
+		}
 	}
+
+	//if err := os.RemoveAll("data/1"); err != nil {
+	//	log.Fatalf("os.RemoveAll error:%v", err)
+	//}
 }
