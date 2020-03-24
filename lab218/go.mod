5b2eb341ecd4301a6ad8b7955feabc4188390d49
liguoqinjim <liguoqinjim23@gmail.com>
2020-03-24 23:25:26 +0800

    update

--- lab218/go.mod
+++ lab218/go.mod
@@ -2,4 +2,7 @@ module lab218
 
 go 1.14
 
-require github.com/silenceper/wechat v1.2.3
+require (
+	github.com/silenceper/wechat v1.2.3
+	github.com/spf13/viper v1.6.2 // indirect
+)
--- lab218/lab001/main.go
+++ lab218/lab001/main.go
@@ -1,17 +1,68 @@
 package main
 
-import(
+import (
+	"flag"
 	"github.com/silenceper/wechat"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
 )
 
+var (
+	appId     string
+	appSecret string
+)
+
+func init() {
+	pflag.StringVarP(&appId, "appId", "i", "appId", "appId")
+	pflag.StringVarP(&appSecret, "appSecret", "s", "appSecret", "appSecret")
+
+	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
+	pflag.Parse()
+	viper.BindPFlags(pflag.CommandLine)
+}
+
 func main() {
-	memCache := cache.NewMemcache("127.0.0.1:11211")
 	config := &wechat.Config{
-		AppID:     "xxx",
-		AppSecret: "xxx",
-		Cache:     memCache = cache.NewMemcache("127.0.0.1:11211"),
+		AppID:     appId,
+		AppSecret: appSecret,
 	}
 	wc := wechat.NewWechat(config)
 
 	wxa := wc.GetMiniProgram()
+
+	_ = wxa
+
+	//if resp, err := wxa.GetWXACodeUnlimit(miniprogram.QRCoder{
+	//	Page:      "",
+	//	Path:      "",
+	//	Scene:     "",
+	//}); err != nil {
+	//	log.Fatalf("wxa.CreateWXAQRCode error:%v", err)
+	//} else {
+	//	filename := "../data/1.png"
+	//
+	//}
+
+	//// CreateWXAQRCode 获取小程序二维码，适用于需要的码数量较少的业务场景
+	//// 文档地址： https://developers.weixin.qq.com/miniprogram/dev/api/createWXAQRCode.html
+	//func (wxa *MiniProgram) CreateWXAQRCode(coderParams QRCoder) (response []byte, err error) {
+	//	return wxa.fetchCode(createWXAQRCodeURL, coderParams)
+	//}
+	//
+	//// GetWXACode 获取小程序码，适用于需要的码数量较少的业务场景
+	//// 文档地址： https://developers.weixin.qq.com/miniprogram/dev/api/getWXACode.html
+	//func (wxa *MiniProgram) GetWXACode(coderParams QRCoder) (response []byte, err error) {
+	//	return wxa.fetchCode(getWXACodeURL, coderParams)
+	//}
+	//
+	//// GetWXACodeUnlimit 获取小程序码，适用于需要的码数量极多的业务场景
+	//// 文档地址： https://developers.weixin.qq.com/miniprogram/dev/api/getWXACodeUnlimit.html
+	//func (wxa *MiniProgram) GetWXACodeUnlimit(coderParams QRCoder) (response []byte, err error) {
+	//	return wxa.fetchCode(getWXACodeUnlimitURL, coderParams)
+	//}
+
+	//
+	//wxa := wc.GetMiniProgram()
+	//
+	//wxa.
 }
