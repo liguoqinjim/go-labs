a5b58f1338f425d481fb7d2a9dbf274683f81bc6
liguoqinjim <liguoqinjim23@gmail.com>
2020-03-26 22:50:04 +0800

    update

--- lab081/lab004/README.md
+++ lab081/lab004/README.md
@@ -1,6 +1,9 @@
 # 给定表示时间的字符串如("2017-02-08")，转换到time
 只要指定parse中用的layout参数就可以解析
 
+## NOTICE
+ - `ParseInLocation`，相较于`Parse`多了时区的选择。为了保险，一般就用ParseInLocation好了
+
 ## 参考资料
 https://studygolang.com/articles/3753
 
--- lab081/lab004/main.go
+++ lab081/lab004/main.go
@@ -7,12 +7,17 @@ import (
 
 func main() {
 	myTime := "2017-02-08"
-	log.Println("myTime=", myTime)
+	log.Println("str=", myTime)
 
 	//t, _ := time.Parse("2006-01-02 15:04:05", "2014-06-15 08:37:18")
 	t, err := time.Parse("2006-01-02", myTime)
 	if err != nil {
 		log.Fatalf("time.Parse error:%v", err)
 	}
+	log.Println("t=", t)
 	log.Printf("year=%d,month=%d,day=%d\n", t.Year(), t.Month(), t.Day())
+
+	//时区
+	t2, err := time.ParseInLocation("2006-01-02", myTime, time.Local)
+	log.Println("t2=", t2)
 }
