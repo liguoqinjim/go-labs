ed2a5ba388935300db0733a5579f8f67fe8239e3
liguoqinjim <liguoqinjim23@gmail.com>
2020-05-01 15:55:27 +0800

    update

--- lab218/lab001/main.go
+++ lab218/lab001/main.go
@@ -3,8 +3,12 @@ package main
 import (
 	"flag"
 	"github.com/silenceper/wechat"
+	"github.com/silenceper/wechat/cache"
+	"github.com/silenceper/wechat/miniprogram"
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
+	"io/ioutil"
+	"log"
 )
 
 var (
@@ -22,26 +26,28 @@ func init() {
 }
 
 func main() {
+	c := cache.NewRedis(&cache.RedisOpts{
+		Host:     "127.0.0.1:6379",
+		Database: 1,
+	})
+
 	config := &wechat.Config{
 		AppID:     appId,
 		AppSecret: appSecret,
+		Cache:     c,
 	}
 	wc := wechat.NewWechat(config)
 
 	wxa := wc.GetMiniProgram()
 
-	_ = wxa
-
-	//if resp, err := wxa.GetWXACodeUnlimit(miniprogram.QRCoder{
-	//	Page:      "",
-	//	Path:      "",
-	//	Scene:     "",
-	//}); err != nil {
-	//	log.Fatalf("wxa.CreateWXAQRCode error:%v", err)
-	//} else {
-	//	filename := "../data/1.png"
-	//
-	//}
+	if resp, err := wxa.GetWXACodeUnlimit(miniprogram.QRCoder{Scene: "123"}); err != nil {
+		log.Fatalf("wxa.CreateWXAQRCode error:%v", err)
+	} else {
+		filename := "../data/1.png"
+		if err := ioutil.WriteFile(filename, resp, 0600); err != nil {
+			log.Fatalf("writeFile error:%v", err)
+		}
+	}
 
 	//// CreateWXAQRCode 获取小程序二维码，适用于需要的码数量较少的业务场景
 	//// 文档地址： https://developers.weixin.qq.com/miniprogram/dev/api/createWXAQRCode.html
