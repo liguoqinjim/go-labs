0a2f35488699571f718cf8a460c9325fcd11e68e
liguoqinjim <liguoqinjim23@gmail.com>
2021-02-24 15:39:08 +0800

    update

--- lab036/lab006/main.go
+++ lab036/lab006/main.go
@@ -6,8 +6,16 @@ import (
 )
 
 func main() {
+	defer func() {
+		if r := recover(); r != nil {
+			log.Fatalf("panic get error:%v", r)
+		}
+	}()
+
 	log.Println("log01")
 	log.Infof("this is info")
 	log.Debugf("this is debug")
 	log.Errorf("this is error")
+
+	log.Panicf("this is a panic")
 }
