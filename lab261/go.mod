d13b75389a068885d963b3edefa3553c4052b66b
liguoqinjim <liguoqinjim23@gmail.com>
2021-03-16 16:16:45 +0800

    261

--- lab261/go.mod
+++ lab261/go.mod
@@ -2,4 +2,7 @@ module lab261
 
 go 1.15
 
-require github.com/dhowden/tag v0.0.0-20201120070457-d52dcb253c63 // indirect
+require (
+	github.com/dhowden/tag v0.0.0-20201120070457-d52dcb253c63 // indirect
+	golang.org/x/text v0.3.5 // indirect
+)
--- lab261/lab001/main.go
+++ lab261/lab001/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"github.com/dhowden/tag"
+	"golang.org/x/text/encoding/simplifiedchinese"
+	"golang.org/x/text/transform"
+	"io/ioutil"
 	"log"
 	"os"
 )
@@ -11,8 +15,8 @@ func main() {
 }
 
 func demo() {
-	//f, err := os.Open("../data/output.mp3")
-	f, err := os.Open("../../lab262/data/test.mp3")
+	f, err := os.Open("../data/output.mp3")
+	//f, err := os.Open("../../lab262/data/test.mp3")
 	if err != nil {
 		log.Fatalf("os.Open error:%v", err)
 	}
@@ -24,4 +28,20 @@ func demo() {
 	log.Print(m.Format()) // The detected format.
 	log.Print(m.Title())  // The title of the track (see Metadata interface for more details).
 	log.Println(m.Album())
+	albumUtf, _ := GBKToUTF8([]byte(m.Album()))
+	log.Printf("%s", albumUtf)
+
+	log.Println(m.Artist())
+	artistUtf, _ := GBKToUTF8([]byte(m.Artist()))
+	log.Printf("%s", artistUtf)
+}
+
+func GBKToUTF8(s []byte) ([]byte, error) {
+	reader := transform.NewReader(bytes.NewReader(s), simplifiedchinese.GBK.NewDecoder())
+	d, e := ioutil.ReadAll(reader)
+	if e != nil {
+		return nil, e
+	}
+
+	return d, nil
 }
